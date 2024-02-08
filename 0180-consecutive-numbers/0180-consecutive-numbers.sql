0d6d5c74c16b7e2f55aa70b8deda2838ce021f69
Arunita S <114884556+ASarkar0611@users.noreply.github.com>
2024-02-07 22:10:17 -0500

    Time: 791 ms (96.78%), Space: 0B (100.00%) - LeetHub

--- 0180-consecutive-numbers/0180-consecutive-numbers.sql
+++ 0180-consecutive-numbers/0180-consecutive-numbers.sql
@@ -1,5 +1,12 @@
 # Write your MySQL query statement below
-select distinct l1.num as ConsecutiveNums
-from Logs l1
-inner join Logs l2 on l2.num = l1.num and l2.id = l1.id - 1
-inner join Logs l3 on l3.num = l2.num and l3.id = l2.id - 1;
+with connum as (select distinct case
+        when num = prev_num and next_num = num then num
+       end as ConsecutiveNums
+from
+(select *,
+       lag(num, 1) over (order by id) as prev_num,
+       lead(num, 1) over (order by id) as next_num
+from logs) t)
+
+#select * from connum
+select ConsecutiveNums from connum where ConsecutiveNums is not null
