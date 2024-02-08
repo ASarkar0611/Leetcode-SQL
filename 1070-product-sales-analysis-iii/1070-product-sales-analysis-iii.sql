78cb9b9a3d51d59727126d661ed2b20219fd7eb6
Arunita S <114884556+ASarkar0611@users.noreply.github.com>
2024-02-08 10:26:16 -0500

    Time: 1688 ms (67.32%), Space: 0B (100.00%) - LeetHub

--- 1070-product-sales-analysis-iii/1070-product-sales-analysis-iii.sql
+++ 1070-product-sales-analysis-iii/1070-product-sales-analysis-iii.sql
@@ -1,8 +1,11 @@
 # Write your MySQL query statement below
-with t as (
-select s.product_id, s.year as first_year, s.quantity, s.price,
-    dense_rank() over (partition by s.product_id order by s.year) as rn
-from sales s)
+with first_year_prod as
+(select product_id, min(year) as first_year
+from sales
+group by product_id)
 
-select product_id, first_year, quantity, price from t
-where rn = 1;
+select fyp.product_id, fyp.first_year, s.quantity,
+       s.price
+from first_year_prod fyp, sales s
+where fyp.product_id = s.product_id
+and fyp.first_year = s.year
