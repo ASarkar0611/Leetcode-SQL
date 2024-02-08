c9a3215a83db420e30cece3d0b993e6741e90140
Arunita S <114884556+ASarkar0611@users.noreply.github.com>
2024-02-07 22:14:49 -0500

    Time: 1110 ms (79.60%), Space: 0B (100.00%) - LeetHub

--- 0184-department-highest-salary/0184-department-highest-salary.sql
+++ 0184-department-highest-salary/0184-department-highest-salary.sql
@@ -1,9 +1,10 @@
 # Write your MySQL query statement below
-select d.name as 'Department', tb.name as 'Employee',
-tb.salary as 'Salary'
-from
-(select e.departmentId, e.name, e.salary,
-dense_rank() over (partition by e.departmentId order by e.salary desc) as r
-from employee e) tb
-inner join Department d on tb.departmentId = d.id
-where tb.r = 1;
+with department_rank as
+(select *,
+        dense_rank() over (partition by departmentId order by salary desc) as rn
+from employee)
+
+select d.name as Department, dr.name as Employee, dr.salary as Salary
+from department_rank dr, department d
+where dr.departmentId = d.id
+and dr.rn = 1
